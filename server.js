327b800899de7267cf193ed6031942b37084ccfd
bohjorth <bo@hjorthene.dk>
2026-08-09 19:22:02 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -2,19 +2,42 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const port = 3000;
+const port = 8080;
+const dataFile = path.join(__dirname, 'data.json');
 
-http.createServer((req,res)=>{
-  let file = req.url === '/' ? '/index.html' : req.url;
-  let filePath = path.join(__dirname, file);
-
+function serveFile(res, filePath, type="text/html"){
   fs.readFile(filePath, (err,data)=>{
-    if(err){
-      res.writeHead(404);
-      res.end("Not found");
-      return;
-    }
-    res.writeHead(200);
+    if(err){ res.writeHead(404); return res.end("Not found"); }
+    res.writeHead(200, {"Content-Type": type});
     res.end(data);
   });
+}
+
+http.createServer((req,res)=>{
+  if(req.url === '/api/jumbo' && req.method === 'GET'){
+    fs.readFile(dataFile, (err,data)=>{
+      res.writeHead(200, {"Content-Type":"application/json"});
+      res.end(data);
+    });
+    return;
+  }
+
+  if(req.url === '/api/jumbo' && req.method === 'POST'){
+    let body="";
+    req.on('data', chunk => body+=chunk);
+    req.on('end', ()=>{
+      fs.writeFileSync(dataFile, body);
+      res.writeHead(200);
+      res.end("OK");
+    });
+    return;
+  }
+
+  let file = req.url === '/' ? '/index.html' : req.url;
+  let filePath = path.join(__dirname, file);
+  let ext = path.extname(filePath);
+
+  let types = {".js":"text/javascript",".css":"text/css",".json":"application/json"};
+  serveFile(res, filePath, types[ext] || "text/html");
+
 }).listen(port, ()=>console.log("Running on "+port));
