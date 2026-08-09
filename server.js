34cdc644f6d10369e6cf31ea02a83115a4441792
bohjorth <bo@hjorthene.dk>
2026-08-09 19:35:09 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const https = require('https');
 
 const port = 8080;
 const dataFile = path.join(__dirname, 'data.json');
+const coverDir = path.join(__dirname, 'covers');
+
+if (!fs.existsSync(coverDir)) fs.mkdirSync(coverDir);
 
 function serveFile(res, filePath, type="text/html"){
   fs.readFile(filePath, (err,data)=>{
@@ -13,7 +17,62 @@ function serveFile(res, filePath, type="text/html"){
   });
 }
 
+function fetchPage(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => resolve(data));
+    }).on('error', reject);
+  });
+}
+
+function downloadFile(url, filePath) {
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(filePath);
+    https.get(url, (res) => {
+      res.pipe(file);
+      file.on('finish', () => file.close(resolve));
+    }).on('error', reject);
+  });
+}
+
+async function getRealCover(id, res) {
+  const file = path.join(coverDir, id + '.jpg');
+
+  if (fs.existsSync(file)) {
+    return serveFile(res, file, "image/jpeg");
+  }
+
+  try {
+    const url = `https://comicwiki.dk/wiki/Jumbobog_${id}`;
+    const html = await fetchPage(url);
+
+    const match = html.match(/<img[^>]+src="([^"]+)"/);
+
+    if (!match) throw "no image";
+
+    let imgUrl = match[1];
+
+    if (imgUrl.startsWith('//')) {
+      imgUrl = 'https:' + imgUrl;
+    }
+
+    await downloadFile(imgUrl, file);
+
+    return serveFile(res, file, "image/jpeg");
+
+  } catch (e) {
+    console.log("Cover fejl:", id, e);
+    res.writeHead(302, {
+      Location: `https://picsum.photos/300/450?random=${id}`
+    });
+    res.end();
+  }
+}
+
 http.createServer((req,res)=>{
+
   if(req.url === '/api/jumbo' && req.method === 'GET'){
     fs.readFile(dataFile, (err,data)=>{
       res.writeHead(200, {"Content-Type":"application/json"});
@@ -31,6 +90,11 @@ http.createServer((req,res)=>{
       res.end("OK");
     });
     return;
+  }
+
+  if(req.url.startsWith('/api/cover/')){
+    const id = req.url.split('/').pop();
+    return getRealCover(id, res);
   }
 
   let file = req.url === '/' ? '/index.html' : req.url;
